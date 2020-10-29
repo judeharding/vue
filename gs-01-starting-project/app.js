da6ccb3d35cf8e2e9b700e76953ee031411b50f9
judeharding <jharding@genesisgroupusa.com>
2020-10-29 16:00:33 -0400

    cdn vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,17 +1,45 @@
+Vue.createApp({
+    // get the data from the field entered
+    data() {
+        // return an object
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    // methods
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
+
+
+
+
+
+
+
+
+
 // to add stuff to the list, you need 3 constants:
 // access to button, access to field, access to list
 
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+//VANILLAJS
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
 
-function addGoal() {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value = '';
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
 
-}
-buttonEl.addEventListener('click', addGoal);
+// }
+// buttonEl.addEventListener('click', addGoal);
+
+// VUEJS
