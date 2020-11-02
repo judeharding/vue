83eb7841740405766a8e8070b0585297b81af9a6
judeharding <jharding@genesisgroupusa.com>
2020-11-02 15:51:18 -0500

    wip sect. 2

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    // key option is data property key
+    data: function () {
+        // the data property always returns an object 
+        return {
+            courseGoal: 'Finish the Vue class',
+            vueLink: 'https://www.vuejs.org'
+        };
+    },
+    // method option takes an object full of method
+    methods: {
+        // all of the method outputs are functions
+        outputGoal: function () {
+            const randNumber = Math.random();
+            if (randNumber < .5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Mastered vue!';
+            }
+        }
+    }
+
+});
+
+// vue control just a section via css selcetor
+app.mount('#user-goal');
+
