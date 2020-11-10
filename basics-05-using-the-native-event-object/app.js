1f6996f81b3d0d55d890c2ee45db80823054c468
judeharding <jharding@genesisgroupusa.com>
2020-11-10 12:16:48 -0500

    to vid  24

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+
+    reset() {
+      this.name = '';
+
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    }
+  }
+});
+
+app.mount('#events');
